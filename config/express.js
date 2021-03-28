a7422708a9868acfb74a555a99cd8940af9a05af
nbt25 <nbt25@uclive.ac.nz>
2021-03-29 08:47:50 +1300

    changing config to work with photos

--- config/express.js
+++ config/express.js
@@ -12,6 +12,9 @@ module.exports = function () {
     app.use(allowCrossOriginRequestsMiddleware);
     app.use(bodyParser.json());
     app.use(bodyParser.raw({ type: 'text/plain' }));  // for the /executeSql endpoint
+    app.user(bodyParser.raw({type: 'image/jpeg', limit: '200mb'}))
+    app.user(bodyParser.raw({type: 'image/png', limit: '200mb'}))
+    app.user(bodyParser.raw({type: 'image/gif', limit: '200mb'}))
 
     // DEBUG (you can remove these)
     app.use((req, res, next) => {
